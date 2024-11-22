885f95536b42f462d9f4259b7f75cf3854d0ec67
Fan <Fan@example.com>
2024-11-22 10:00:40 +0800

    output version

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def def_route(route, template):
 for route, template in routes_name.items() : app.route(route)(def_route(route, template))
 
 # 啟動
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__': app.run(host='0.0.0.0', port=8)
